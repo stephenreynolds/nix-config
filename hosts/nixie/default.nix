b12fc7411f20fdd190de385e7affc15884e4519d
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-14 00:03:09 -0400

    feat(bluetooth): add bluetooth

--- hosts/nixie/default.nix
+++ hosts/nixie/default.nix
@@ -14,6 +14,7 @@
     ../common/optional/pipewire.nix
     ../common/optional/quietboot.nix
     ../common/optional/nextdns.nix
+    ../common/optional/bluetooth.nix
     ../common/optional/printing.nix
   ];
 
