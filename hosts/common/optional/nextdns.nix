e713a5370262d5ebb6b096d00c0ceb00c7a2e678
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-13 21:00:35 -0400

    fix(nextdns): temporarily disable dnssec to fix name resolution

--- hosts/common/optional/nextdns.nix
+++ hosts/common/optional/nextdns.nix
@@ -1,14 +1,19 @@
-{
-  networking.nameservers = [
+let
+  nameservers = [
     "45.90.28.0#***REMOVED***.dns.nextdns.io"
     "2a07:a8c0::#***REMOVED***.dns.nextdns.io"
     "45.90.30.0#***REMOVED***.dns.nextdns.io"
     "2a07:a8c1::#***REMOVED***.dns.nextdns.io"
   ];
+in
+{
+  networking.nameservers = nameservers;
+
   services.resolved = {
     enable = true;
-    dnssec = "true";
+    dnssec = "false";
     domains = [ "~." ];
+    fallbackDns = nameservers;
     extraConfig = ''
       DNSOverTLS=yes
     '';
