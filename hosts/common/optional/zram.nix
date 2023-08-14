f7acc33bf62ef0ac792b8d70dd1fca5cca5dde17
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-14 00:28:22 -0400

    feat(zram): fix invalid syntax

--- hosts/common/optional/zram.nix
+++ hosts/common/optional/zram.nix
@@ -6,9 +6,9 @@
 
   # Source: https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
   boot.kernel.sysctl = {
-    "vm.swappiness = 180";
-    "vm.watermark_boost_factor = 0";
-    "vm.watermark_scale_factor = 125";
-    "vm.page-cluster = 0";
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
   };
 }
