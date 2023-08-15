b6d823c8b2feb1ac3d6e9a0bbb27af0be3cda533
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-14 22:37:38 -0400

    feat(waybar): change waybar config

--- home/stephen/features/desktop/common/wayland-wm/waybar.nix
+++ home/stephen/features/desktop/common/wayland-wm/waybar.nix
@@ -1,4 +1,7 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
+let
+  pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
+in
 {
   programs.waybar = {
     enable = true;
@@ -7,23 +10,154 @@
     });
     systemd.enable = true;
     settings = {
-      secondary = {
+      primary = {
         mode = "dock";
         layer = "top";
-        height = 32;
-        width = 100;
-        margin = "6";
-        position = "bottom";
-        modulesacenter = (lib.optionals config.wayland.windowManager.hyprland.enable [
+        height = 28;
+        position = "top";
+        output = builtins.map (m:  m.name) (builtins.filter (m: !m.noBar) config.monitors);
+        modules-left = (lib.optionals config.wayland.windowManager.hyprland.enable [
           "wlr/workspaces"
+          "cpu"
+          "memory"
         ]);
+        modules-center = [
+        ];
+        modules-right = [
+          "network"
+          "pulseaudio"
+          "tray"
+          "clock"
+        ];
+
         "wlr/workspaces" = {
           on-click = "activate";
         };
+        clock = {
+          format = "{:%a %b %d  %I:%M %p}";
+          timezones = [
+            "America/Detroit"
+            "Asia/Tokyo"
+          ];
+          tooltip = true;
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "month";
+            mode-mon-col = 3;
+            on-scroll = 1;
+            on-click-right = "mode";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b>{}</b></span>";
+            };
+          };
+          actions = {
+            on-click-right = "mode";
+            on-click-forward = "tz_up";
+            on-click-backward = "tz_down";
+            on-scroll-up = "shift_up";
+            on-scroll-down = "shift_down";
+          };
+        };
+        tray = {
+          spacing = 10;
+          reverse-direction = true;
+        };
+        cpu = {
+          interval = 1;
+          format = " {}%";
+          max-length = 10;
+          on-clock = "kitty -e btop";
+        };
+        memory = {
+          interval = 5;
+          format = " {}%";
+          format-alt = " {used}G";
+          states = {
+            warning = 16;
+            critical = 24;
+          };
+        };
+        network = {
+          format-wifi = "直";
+          format-ethernet = "";
+          on-click = "kitty -e nmtui";
+          format-disconnected = "睊";
+        };
+      };
+      pulseaudio = {
+        format = "{icon}  {volume}%";
+        format-muted = "   0%";
+        format-icons = {
+          headphone = "󰋋";
+          headset = "󰋎";
+          portable = "";
+          default = [ "" "" "" ];
+        };
+        on-click = pavucontrol;
+      };
+      network = {
+        interval = 3;
+        format-wifi = "   {essid}";
+        format-ethernet = "󰈁 Connected";
+        format-disconnected = "";
+        tooltip-format = ''
+          {ifname}
+          {ipaddr}/{cidr}
+          Up: {bandwidthUpBits}
+          Down: {bandwidthDownBits}'';
+        on-click = "";
+      };
+    };
+    style = ''
+      * {
+        font-family: ${config.fontProfiles.regular.family}, ${config.fontProfiles.monospace.family};;
+        font-size: 10pt;
+        padding: 0 8px;
       }
-    };
-    style = {
 
-    };
+      .modules-right {
+        margin-right: -15px;
+      }
+
+      .modules-left {
+        margin-left: -15px;
+      }
+
+      window#waybar.top {
+        opacity: 0.95;
+        padding: 0;
+        border-radius: 10px;
+      }
+      window#waybar.bottom {
+        opacity: 0.90;
+        border-radius: 10px;
+      }
+
+      window#waybar {
+      }
+
+      #workspaces button {
+        margin: 4px;
+      }
+      #workspaces button.hidden {
+      }
+      #workspaces button.focused,
+      #workspaces button.active {
+      }
+
+      #clock {
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+
+      #tray {
+      }
+    '';
   };
 }
