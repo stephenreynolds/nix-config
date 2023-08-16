c91645e67141d0421323ac2fbf2e66117f4b0563
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-15 20:01:31 -0400

    feat(nvim): add plugin dependencies

--- home/stephen/features/nvim/default.nix
+++ home/stephen/features/nvim/default.nix
@@ -8,6 +8,19 @@
       nodejs
       fzf
       unzip
+      sqlite
+      luajitPackages.sqlite
+      wget
+      go
+      cargo
+      php
+      luarocks
+      jdk17
+      python3
+      python311Packages.pip
+      julia
+      ruby
+      tree-sitter
     ];
   };
 }
