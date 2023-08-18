64957532a5dfab37933d87db33130ed1e8a26e13
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-18 16:20:52 -0400

    fix(proton): enable raytracing support

--- home/stephen/features/games/default.nix
+++ home/stephen/features/games/default.nix
@@ -4,5 +4,13 @@
     ./lutris.nix
     ./steam.nix
   ];
+
   home.packages = with pkgs; [ gamescope ];
+
+  home.sessionVariables = {
+    VKD3D_CONFIG = "dxr11,dxr";
+    PROTON_ENABLE_NVAPI = 1;
+    PROTON_ENABLE_NGX_UPDATER = 1;
+    PROTON_HIDE_NVIDIA_GPU = 0;
+  };
 }
