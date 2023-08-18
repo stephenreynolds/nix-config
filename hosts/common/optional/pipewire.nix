fdcaf3443aaea190668966905a1149084d17a46a
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-18 13:25:46 -0400

    feat(pipewire): remove latency fix

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -8,15 +8,4 @@
     pulse.enable = true;
     jack.enable = true;
   };
-
-  environment.etc = {
-    "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
-      context.properties = {
-        default.clock.rate = 48000
-        default.clock.quantum = 32
-        default.clock.min-quantum = 32
-        default.clock.max-quantum = 32
-      }
-    '';
-  };
 }
