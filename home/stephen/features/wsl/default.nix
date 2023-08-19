1f5ce25a92637504c153a0b5e92f5d5e85d78d9d
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-19 09:52:33 -0400

    Revert "feat(wsl): add config for WSL"
    This reverts commit 2b42f9934050cd2142d333dd9ba68390eee42427.

--- home/stephen/features/wsl/default.nix
+++
@@ -1,5 +0,0 @@
-{
-  programs.git.extraConfig = {
-    credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
-  };
-}
--- home/stephen/wsl.nix
+++ home/stephen/wsl.nix
@@ -1,6 +1,9 @@
 {
   imports = [
     ./global
-    ./features/wsl
   ];
+
+  programs.git.extraConfig = {
+    credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+  };
 }
