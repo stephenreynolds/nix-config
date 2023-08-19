2b42f9934050cd2142d333dd9ba68390eee42427
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-19 09:25:45 -0400

    feat(wsl): add config for WSL

--- home/stephen/wsl.nix
+++ home/stephen/wsl.nix
@@ -1,9 +1,6 @@
 {
   imports = [
     ./global
+    ./features/wsl
   ];
-
-  programs.git.extraConfig = {
-    credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
-  };
 }
