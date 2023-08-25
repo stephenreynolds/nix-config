e4ba49aef69d0976c65951e287b7f978f36cf591
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-25 16:52:19 -0400

    feat(impermanence): set up ephemeral btrfs on nixie

---
+++ hosts/common/optional/ephemeral-btrfs.nix
@@ -0,0 +1,66 @@
+{ lib, config, ... }:
+let
+  hostname = config.networking.hostName;
+  wipeScript = ''
+    mkdir -p /tmp
+    MNTPOINT=$(mktemp -d)
+    (
+      mount -t btrfs -o subvol=/ /dev/disk/by-label/${hostname} "$MNTPOINT"
+      trap 'umount "$MNTPOINT"' EXIT
+
+      echo "Creating needed directories"
+      mkdir -p "$MNTPOINT"/persist/var/{log,lib/{nixos,systemd}}
+
+      echo "Cleaning root subvolume"
+      btrfs subvolume list -o "$MNTPOINT/root" | cut -f9 -d ' ' |
+      while read -r subvolume; do
+        btrfs subvolume delete "$MNTPOINT/$subvolume"
+      done && btrfs subvolume delete "$MNTPOINT/root"
+
+      echo "Restoring blank subvolume"
+      btrfs subvolume snapshot "$MNTPOINT/root-blank" "$MNTPOINT/root"
+    )
+  '';
+  phase1Systemd = config.boot.initrd.systemd.enable;
+in
+{
+  boot.initrd = {
+    supportedFilesystems = [ "btrfs" ];
+    postDeviceCommands = lib.mkIf (!phase1Systemd) (lib.mkBefore wipeScript);
+    systemd.services.restore-root = lib.mkIf phase1Systemd {
+      description = "Rollback btrfs rootfs";
+      wantedBy = [ "initrd.target" ];
+      requires = [
+        "dev-desk-by\\x2dlabel-${hostname}.device"
+      ];
+      after = [
+        "dev-disk-by\\x2dlabel-${hostname}.device"
+        "systemd-cryptsetup@${hostname}.service"
+      ];
+      before = [ "sysroot.mount" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = wipeScript;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress-force=zstd:2" ];
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "noatime" "compress-force=zstd:2" ];
+    };
+
+    "/persist" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress-force=zstd:2" ];
+    };
+  };
+}
--- hosts/nixie/hardware-configuration.nix
+++ hosts/nixie/hardware-configuration.nix
@@ -1,28 +1,14 @@
 {
+  imports = [
+    ../common/optional/ephemeral-btrfs.nix
+  ];
+
   boot = {
     initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
     kernelModules = [ "kvm-intel" ];
   };
 
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/ROOT";
-      fsType = "btrfs";
-      options = [ "subvol=@" "noatime" "compress-force=zstd" "space_cache=v2" ];
-    };
-
-    "/home" = {
-      device = "/dev/disk/by-label/ROOT";
-      fsType = "btrfs";
-      options = [ "subvol=@home" "noatime" "compress-force=zstd" "space_cache=v2" ];
-    };
-
-    "/nix" = {
-      device = "/dev/disk/by-label/ROOT";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" "noatime" "compress-force=zstd" "space_cache=v2" ];
-    };
-
     "/boot" = {
       device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
