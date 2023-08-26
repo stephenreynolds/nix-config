d2a2fbfec0d0ba818290e84f104e597bdf90caa2
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-26 15:52:06 -0400

    refactor(hyprland): move substituter to hyprland.nxi

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -11,4 +11,9 @@
       pkgs.xdg-desktop-portal-gtk
     ];
   };
+
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
