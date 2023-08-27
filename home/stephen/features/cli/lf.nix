dfaf75bcbd5d0c7e4eedc64e001a0f90bfc8b139
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-27 14:53:23 -0400

    feat(lf): dynamically set number of columns

--- home/stephen/features/cli/lf.nix
+++ home/stephen/features/cli/lf.nix
@@ -83,5 +83,17 @@ in
     settings = {
       cleaner = "${cleaner}";
     };
+    extraConfig = ''
+      ''${{
+          w=$(tput cols)
+          if [ $w -le 80 ]; then
+              lf -remote "send $id set ratios 1:2"
+          elif [ $w -le 160 ]; then
+              lf -remote "send $id set ratios 1:2:3"
+          else
+              lf -remote "send $id set ratios 1:2:3:5"
+          fi
+      }}
+    '';
   };
 }
