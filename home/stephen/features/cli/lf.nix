6205d9e634e7328bfa834ace37c054a8d97128ce
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-27 13:20:33 -0400

    feat(lf): add video preview

--- home/stephen/features/cli/lf.nix
+++ home/stephen/features/cli/lf.nix
@@ -1,6 +1,45 @@
 { pkgs, ... }:
 let
-  previewer = pkgs.writeShellScript "previewer.sh" ''
+  vidthumb = pkgs.writeShellScript "vidthumb" ''
+    #!/usr/bin/env bash
+
+    if ! [ -f "$1" ]; then
+      exit 1
+    fi
+
+    cache="$HOME/.cache/vidthumb"
+    index="$cache/index.json"
+    movie="$(realpath "$1")"
+
+    mkdir -p "$cache"
+
+    if [ -f "$index" ]; then
+      thumbnail="$(jq -r ". \"$movie\"" <"$index")"
+      if [[ "$thumbnail" != "null" ]]; then
+        if [[ ! -f "$cache/$thumbnail" ]]; then
+          exit 1
+        fi
+        echo "$cache/$thumbnail"
+        exit 0
+      fi
+    fi
+
+    thumbnail="$(uuidgen).jpg"
+
+    if ! ffmpegthumbnailer -i "$movie" -o "$cache/$thumbnail" -s 0 2>/dev/null; then
+      exit 1
+    fi
+
+    if [[ ! -f "$index" ]]; then
+      echo "{\"$movie\": \"$thumbnail\"}" >"$index"
+    fi
+    json="$(jq -r --arg "$movie" "$thumbnail" ". + {\"$movie\": \"$thumbnail\"}" <"$index")"
+    echo "$json" >"$index"
+
+    echo "$cache/$thumbnail"
+  '';
+
+  previewer = pkgs.writeShellScript "previewer" ''
     #!/usr/bin/env bash
     file=$1
     w=$2
@@ -8,15 +47,22 @@ let
     x=$4
     y=$5
 
-    if [[ "$( file -Lb --mime-type "$file")" =~ ^image ]]; then
-        kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-        exit 1
+    filetype="$( file -Lb --mime-type "$file")"
+
+    if [[ "$filetype" =~ ^image ]]; then
+      kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
+      exit 1
+    fi
+
+    if [[ "$filetype" =~ ^video ]]; then
+       kitty +kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$(${vidthumb} "$file")" < /dev/null > /dev/tty
+       exit 1
     fi
 
     pistol "$file"
   '';
 
-  cleaner = pkgs.writeShellScript "cleaner.sh" ''
+  cleaner = pkgs.writeShellScript "cleaner" ''
     #!/usr/bin/env bash
     kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
   '';
@@ -35,5 +81,6 @@ in
   home.packages = with pkgs; [
     file
     pistol
+    ffmpegthumbnailer
   ];
 }
