e8e77dab5bef109f4392efbbfc67dfd9f6d9ed9a
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-27 18:59:36 -0400

    feat(pass): add pass

--- home/stephen/nixie.nix
+++ home/stephen/nixie.nix
@@ -2,6 +2,7 @@
   imports = [
     ./global
     ./features/desktop/hyprland
+    ./features/pass
     ./features/games
     ./features/productivity
     ./features/music
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,4 +2,5 @@
   fonts = import ./fonts.nix;
   monitors = import ./monitors.nix;
   polkit-gnome = import ./polkit-gnome.nix;
+  pass-secret-service = import ./pass-secret-service.nix;
 }
