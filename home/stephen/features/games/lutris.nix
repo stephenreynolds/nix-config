eabf9269266fea78cbbe15e58b774364412cdb5c
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-28 14:45:05 -0400

    feat(lutris): persist lutris

--- home/stephen/features/games/lutris.nix
+++ home/stephen/features/games/lutris.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = [ pkgs.lutris ];
+
+  home.persistence = {
+    "/persist/home/stephen" = {
+      allowOther = true;
+      directories = [
+        ".config/lutris"
+        ".local/share/lutris"
+      ];
+    };
+  };
 }
