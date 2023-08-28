4054c062195dff7f435fa9a03f66898bc43bdc0e
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-28 00:52:11 -0400

    feat(swww): set static wallpaper

--- home/stephen/features/desktop/common/wayland-wm/swww.nix
+++ home/stephen/features/desktop/common/wayland-wm/swww.nix
@@ -1,43 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
-  wallpapers-path = "/home/stephen/Pictures/Wallpapers/Slideshow";
-
-  wallpaper-randomizer = pkgs.writeShellScript "wallpaper-randomizer" ''
-    # Edit bellow to control the images transition
-    TRANSITION_FPS=30
-    TRANSITION_STEP=90
-    TRANSITION_TYPE=random
-
-    # This controls (in seconds) when to switch to the next image
-    INTERVAL=3600
-
-    while true; do
-      ${pkgs.findutils}/bin/find "${wallpapers-path}" \
-        | while read -r img; do
-          echo "$((RANDOM % 1000)):$img"
-        done \
-        | ${pkgs.coreutils}/bin/sort -n | ${pkgs.coreutils}/bin/cut -d':' -f2- \
-        | while read -r img; do
-          ${pkgs.swww}/bin/swww img "$img" \
-            --transition-type $TRANSITION_TYPE \
-            --transition-step $TRANSITION_STEP \
-            --transition-fps $TRANSITION_FPS
-          ${pkgs.coreutils}/bin/sleep $INTERVAL
-        done
-    done
-  '';
+  wallpaper-path = "${config.xdg.configHome}/wallpaper";
 in
 {
   systemd.user.services.swww = {
     Unit = {
-      Description = "Wayland compositor daemon";
+      Description = "Wayland wallpaper daemon";
       PartOf = [ "graphical-session.target" ];
     };
     Service = {
       ExecStart = "${pkgs.swww}/bin/swww-daemon";
-      ExecStartPost = "${wallpaper-randomizer}";
+      ExecStartPost = "${pkgs.swww}/bin/swww img '${wallpaper-path}'";
       Restart = "on-failure";
     };
     Install.WantedBy = [ "graphical-session.target" ];
+  };
+
+  home.persistence = {
+    "/persist/home/stephen".files = [ ".config/wallpaper" ];
   };
 }
