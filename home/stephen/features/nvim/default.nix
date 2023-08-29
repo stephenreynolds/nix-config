6d3059f35901564a06cd75357f615a8d3b0d5ee7
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-28 22:51:40 -0400

    feat(neovim): overwrite desktop entry

--- home/stephen/features/nvim/default.nix
+++ home/stephen/features/nvim/default.nix
@@ -20,6 +20,36 @@
     ];
   };
 
+  xdg.desktopEntries = {
+    nvim = {
+      name = "Neovim";
+      genericName = "Text Editor";
+      comment = "Edit text files";
+      exec = "nvim %F";
+      icon = "nvim";
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+      ];
+      terminal = true;
+      type = "Application";
+      categories = [ "Utility" "TextEditor" ];
+    };
+  };
+
   home.sessionVariables = {
     NVIM_SQLITE_PATH = pkgs.sqlite.out;
   };
