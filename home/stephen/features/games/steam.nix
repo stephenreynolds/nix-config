5ca24164e71c9c354819682a5c9769391e14e3a1
Stephen Reynolds <mail@stephenreynolds.dev>
2023-08-30 23:30:53 -0400

    fix(optin-persistence): fix path

--- home/stephen/features/games/steam.nix
+++ home/stephen/features/games/steam.nix
@@ -34,7 +34,7 @@ in
           directory = ".local/share/Steam";
           method = "symlink";
         }
-        "./local/share/applications"
+        ".local/share/applications"
       ];
     };
   };
