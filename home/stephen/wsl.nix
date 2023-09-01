f8141592c3b7ea7f93bfa4a58ff9a5396bb661d9
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-01 18:59:51 -0400

    fix(wsl): disable impermanence

--- home/stephen/wsl.nix
+++ home/stephen/wsl.nix
@@ -1,8 +1,11 @@
+{ lib, ... }:
 {
   imports = [
     ./global
     ./features/productivity/latex.nix
   ];
+
+  home.persistence = lib.mkForce { };
 
   programs.git.extraConfig = {
     credential.helper = "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
