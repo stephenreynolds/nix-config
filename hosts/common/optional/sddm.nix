2d0db440d707acc2fc9cac371c7b99c0e3784b4f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-13 22:29:05 -0400

    feat(sddm): set theme

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -1,8 +1,24 @@
+{ pkgs, ... }:
 {
   services.xserver = {
     enable = true;
     displayManager = {
-      sddm.enable = true;
+      sddm = {
+        enable = true;
+        theme = "chili";
+        autoNumlock = true;
+        settings = {
+          Theme = {
+            CursorTheme = "macOS-BigSur-White";
+            CursorSize = 24;
+          };
+        };
+      };
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    sddm-chili-theme
+    apple-cursor
+  ];
 }
