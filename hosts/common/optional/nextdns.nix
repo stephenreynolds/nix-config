67de71e4fb7fec9a0c3f9173899a319617799848
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-13 22:27:26 -0400

    fix(nextdns): use nextdns service and remove id

--- hosts/common/optional/nextdns.nix
+++ hosts/common/optional/nextdns.nix
@@ -1,21 +1,22 @@
-let
-  nameservers = [
-    "45.90.28.0#***REMOVED***.dns.nextdns.io"
-    "2a07:a8c0::#***REMOVED***.dns.nextdns.io"
-    "45.90.30.0#***REMOVED***.dns.nextdns.io"
-    "2a07:a8c1::#***REMOVED***.dns.nextdns.io"
-  ];
-in
+{ config, ... }:
 {
-  networking.nameservers = nameservers;
-
-  services.resolved = {
+  services.nextdns = {
     enable = true;
-    dnssec = "false";
-    domains = [ "~." ];
-    fallbackDns = nameservers;
-    extraConfig = ''
-      DNSOverTLS=yes
+    arguments = [
+      "-config-file"
+      "${config.sops.templates."nextdns.conf".path}"
+    ];
+  };
+
+  sops.templates."nextdns.conf" = {
+    content = ''
+      profile ${config.sops.placeholder.nextdns-id}
+      cache-size 10MB
+      auto-activate yes
     '';
+  };
+
+  sops.secrets.nextdns-id = {
+    sopsFile = ../secrets.yaml;
   };
 }
