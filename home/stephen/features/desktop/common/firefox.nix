e900c4349411516c248b37182e343d6b805baf89
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-18 09:34:12 -0400

    style(firefox): formatting

--- home/stephen/features/desktop/common/firefox.nix
+++ home/stephen/features/desktop/common/firefox.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ...}:
+{ pkgs, inputs, ... }:
 let
   addons = inputs.firefox-addons.packages.${pkgs.system};
 in
@@ -85,23 +85,23 @@ in
         "browser.tabs.tabmanager.enabled" = false;
       };
       userChrome =
-      (builtins.readFile "${inputs.firefox-onebar}/userChrome.css") +
-      ''
-        /* Hide the close button */
-        .titlebar-buttonbox-container{ display:none }
-        .titlebar-spacer[type="post-tabs"]{ display:none }
+        (builtins.readFile "${inputs.firefox-onebar}/userChrome.css") +
+        ''
+          /* Hide the close button */
+          .titlebar-buttonbox-container{ display:none }
+          .titlebar-spacer[type="post-tabs"]{ display:none }
 
-        /* Remove some elements from the bookmarks menu */
-        .openintabs-menuseparator,
-        .openintabs-menuitem,
-        .bookmarks-actions-menuseparator {
-          display: none !important;
-        }
+          /* Remove some elements from the bookmarks menu */
+          .openintabs-menuseparator,
+          .openintabs-menuitem,
+          .bookmarks-actions-menuseparator {
+            display: none !important;
+          }
 
-        #BMB_searchBookmarks, #BMB_bookmarksShowAllTop, #BMB_viewBookmarksSidebar, #BMB_bookmarksShowAll, #BMB_bookmarksToolbar, html#main-window body box#navigator-toolbox-background toolbox#navigator-toolbox toolbar#nav-bar.browser-toolbar hbox#nav-bar-customization-target.customization-target toolbarbutton#bookmarks-menu-button.toolbarbutton-1.chromeclass-toolbar-additional.subviewbutton-nav menupopup#BMB_bookmarksPopup.cui-widget-panel.cui-widget-panelview.PanelUI-subView menuseparator {
-          display: none;
-        }
-      '';
+          #BMB_searchBookmarks, #BMB_bookmarksShowAllTop, #BMB_viewBookmarksSidebar, #BMB_bookmarksShowAll, #BMB_bookmarksToolbar, html#main-window body box#navigator-toolbox-background toolbox#navigator-toolbox toolbar#nav-bar.browser-toolbar hbox#nav-bar-customization-target.customization-target toolbarbutton#bookmarks-menu-button.toolbarbutton-1.chromeclass-toolbar-additional.subviewbutton-nav menupopup#BMB_bookmarksPopup.cui-widget-panel.cui-widget-panelview.PanelUI-subView menuseparator {
+            display: none;
+          }
+        '';
     };
   };
 
