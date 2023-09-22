669ea76690cad23b1a98db0ddac6daa22096dd77
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-22 17:48:44 -0400

    feat(hyprland/rules): add window rules

--- home/stephen/features/desktop/hyprland/config/window-rules.nix
+++ home/stephen/features/desktop/hyprland/config/window-rules.nix
@@ -1,14 +1,27 @@
 {
   wayland.windowManager.hyprland.extraConfig = ''
+    # ags
     windowrule = float, ^(ags)$
+
+    # XDG Portal
     windowrule = float, ^(xdg-desktop-portal)$
     windowrule = float, ^(xdg-desktop-portal-gnome)$
 
+    # wofi
     windowrule = dimaround, ^(wofi)$
-    windowrule = nofullscreenrequest, ^(steam)$
+
+    # TastyTrade
+    windowrule = workspace empty, ^(tasty.javafx.launcher.LauncherFxApp)$
     windowrule = nofullscreenrequest, ^(tasty.javafx.launcher.LauncherFxApp)$
 
-    ## polkit agent
+    # Steam
+    windowrule = workspace empty, ^(Mailspring)$
+
+    # Steam
+    windowrule = workspace empty, ^(steam)$
+    windowrule = nofullscreenrequest, ^(steam)$
+
+    # polkit agent
     windowrulev2 = float,class:^(lxqt-policykit-agent)$
     windowrulev2 = center,class:^(lxqt-policykit-agent)$
     windowrulev2 = float,class:^(polkit-gnome-authentication-agent-1)$
@@ -16,29 +29,29 @@
     windowrulev2 = float,class:^(polkit-mate-authentication-agent-1)$
     windowrulev2 = center,class:^(polkit-mate-authentication-agent-1)$
 
-    ## browser
+    # browser
     windowrulev2 = idleinhibit fullscreen,class:^(Chromium-browser)$
     windowrulev2 = idleinhibit fullscreen,class:^(Brave-browser)$
     windowrulev2 = idleinhibit fullscreen,class:^(firefox)$
     windowrulev2 = idleinhibit fullscreen,class:^(microsoft-edge)$
     windowrulev2 = float,title:^(Firefox - Sharing Indicator)$
 
-    ## picture-in-picture
+    # picture-in-picture
     windowrulev2 = float,title:^(Picture-in-Picture)$
     windowrulev2 = pin,title:^(Picture-in-Picture)$
     windowrulev2 = noborder,title:^(Picture-in-Picture)$
     windowrulev2 = noshadow,title:^(Picture-in-Picture)$
 
-    ## pavucontrol
+    # pavucontrol
     windowrulev2 = float,class:^(pavucontrol)$
     windowrulev2 = center,class:^(pavucontrol)$
     windowrulev2 = float,class:^(pavucontrol-qt)$
     windowrulev2 = center,class:^(pavucontrol-qt)$
 
-    ## piavpn
+    # piavpn
     windowrulev2 = nofullscreenrequest,class:^(piavpn)$
 
-    ## onedrivegui
+    # onedrivegui
     windowrulev2 = float, title:^(OneDriveGUI)$
     windowrulev2 = move 78% 22%, title:^(OneDriveGUI)$
   '';
