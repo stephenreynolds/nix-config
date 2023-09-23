9a7b075e1a577136e09f5331949625c02884573e
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-23 12:18:10 -0400

    fix(thunderbird): remove settings

--- home/stephen/features/productivity/thunderbird.nix
+++ home/stephen/features/productivity/thunderbird.nix
@@ -3,14 +3,6 @@
     enable = true;
     profiles.stephen = {
       isDefault = true;
-      settings = {
-        mail.biff.play_sound = false;
-        mail.e2ee.auto_disable = true;
-        mail.e2ee.auto_enable = true;
-        mail.uifontsize = 14;
-        mailnews.start_page.enabled = false;
-        network.cookie.cookieBehavior = 1;
-      };
     };
   };
 
