6bb4741dbabbdcfcff81481c895b4d9f91083692
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-24 17:19:45 -0400

    feat(hyprland/options): use default window animation

--- home/stephen/features/desktop/hyprland/config/options.nix
+++ home/stephen/features/desktop/hyprland/config/options.nix
@@ -49,9 +49,7 @@
     animations {
       enabled = true
 
-      bezier = overshot, 0.05, 0.9, 0.1, 1.1
-
-      animation = windows, 1, 2, overshot
+      animation = windows, 1, 2, default
       animation = windowsOut, 1, 2, default, popin 80%
       animation = border, 1, 10, default
       animation = borderangle, 1, 8, default
