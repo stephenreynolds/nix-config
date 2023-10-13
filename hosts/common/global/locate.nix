10be89608853edf2816e14e444dca295c04f819f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-13 17:28:43 -0400

    fix(locate): rename package option to new name

--- hosts/common/global/locate.nix
+++ hosts/common/global/locate.nix
@@ -2,7 +2,7 @@
 {
   services.locate = {
     enable = true;
-    locate = pkgs.plocate;
+    package = pkgs.plocate;
     interval = "hourly";
 
     # Suppress "mlocate and plocate do not support localuser" warning
