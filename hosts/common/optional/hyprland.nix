c80d13fec5457a40f398320896d04adef191d6b6
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-16 19:19:42 -0400

    fix(flake): fix hyprland not using cachix

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -14,9 +14,4 @@
       pkgs.xdg-desktop-portal-gtk
     ];
   };
-
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
 }
