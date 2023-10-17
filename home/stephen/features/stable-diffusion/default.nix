dc71b98fd1c20c90f8c12c18bd02272c523eb03c
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-16 22:07:24 -0400

    feat(stable-diffusion): add comfyui

--- home/stephen/features/stable-diffusion/default.nix
+++ home/stephen/features/stable-diffusion/default.nix
@@ -6,4 +6,15 @@
     icon = ./icon.png;
     type = "Application";
   };
+
+  xdg.desktopEntries.comfyui = {
+    name = "ComfyUI";
+    exec = "kitty -d ${config.home.homeDirectory}/src/cloned/ComfyUI nix develop -c ${config.home.homeDirectory}/src/cloned/ComfyUI/start.sh";
+    type = "Application";
+  };
+
+  nix.settings = {
+    trusted-substituters = [ "https://cuda-maintainers.cachix.org" ];
+    trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+  };
 }
--- home/stephen/global/default.nix
+++ home/stephen/global/default.nix
@@ -23,6 +23,8 @@ in
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
       trusted-users = [ "@wheel" ];
+      trusted-substituters = [ "https://cache.nixos.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
     };
   };
 
