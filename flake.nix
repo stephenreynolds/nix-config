113d0febc0f01fd1531a74cbcc2db69acfd21f46
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-21 17:56:08 -0400

    chore(flake): add nix-gaming cachix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,14 @@
   description = "My NixOS configuration";
 
   nixConfig = {
-    extra-substituters = [ "https://hyprland.cachix.org" ];
-    extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-gaming.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
   };
 
   inputs = {
