c6d9e26b56615c4304e8c16fd2e51be706b45e55
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-23 00:13:01 -0400

    feat(hyprland/workspaces): use workspace rule to autostart terminal in special workspace

--- home/stephen/features/desktop/hyprland/config/autostart.nix
+++ home/stephen/features/desktop/hyprland/config/autostart.nix
@@ -1,13 +1,11 @@
 { config, lib, pkgs, inputs, ... }:
 let
   primaryMonitor = lib.findSingle (m: m.primary) "DP-1" "DP-1" config.monitors;
-  terminal = config.home.sessionVariables.TERMINAL;
   ags = "${inputs.ags.packages.${pkgs.system}.default}/bin/ags";
 in
 {
   wayland.windowManager.hyprland.extraConfig = ''
     exec = ${ags} -q; sleep 0.5; ${ags}
     exec-once = hyprctl dispatch focusmonitor ${primaryMonitor.name}
-    exec-once = [workspace special silent;group new] ${terminal}
   '';
 }
--- home/stephen/features/desktop/hyprland/config/workspaces.nix
+++ home/stephen/features/desktop/hyprland/config/workspaces.nix
@@ -1,9 +1,10 @@
 { lib, config, ... }:
 let
   primaryMonitor = lib.findSingle (m: m.primary) "DP-1" "DP-1" config.monitors;
+  terminal = config.home.sessionVariables.TERMINAL;
 in
 {
   wayland.windowManager.hyprland.extraConfig = ''
-    workspace = special, monitor:${primaryMonitor.name}
+    workspace = special, monitor:${primaryMonitor.name}, on-created-empty:[group new] ${terminal}
   '';
 }
