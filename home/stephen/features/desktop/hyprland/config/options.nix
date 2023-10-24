56329b3ee9446624df81288b0afa0a3baa940c85
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-24 01:56:05 -0400

    feat(hyprland/options): remove and reduce animations

--- home/stephen/features/desktop/hyprland/config/options.nix
+++ home/stephen/features/desktop/hyprland/config/options.nix
@@ -48,13 +48,12 @@
     animations {
       enabled = true
 
-      animation = windows, 1, 2, default
-      animation = windowsOut, 1, 2, default, popin 80%
-      animation = border, 1, 10, default
-      animation = borderangle, 1, 8, default
-      animation = fade, 1, 2, default
-      animation = workspaces, 1, 2, default
-      animation = specialWorkspace, 1, 2, default, slidevert
+      animation = windows, 0, 2, default
+      animation = windowsOut, 0, 2, default, popin 80%
+      animation = border, 0
+      animation = fade, 0, 2, default
+      animation = workspaces, 0, 2, default
+      animation = specialWorkspace, 1, 1, default, slidevert
     }
 
     # https://wiki.hyprland.org/Configuring/Dwindle-Layout/#config
