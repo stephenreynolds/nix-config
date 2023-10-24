29bf57a127910a2c9f3fabcc5d91ecb9b8f6e6cd
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-24 00:23:30 -0400

    feat(cli/git): add aliases

--- home/stephen/features/cli/git.nix
+++ home/stephen/features/cli/git.nix
@@ -15,6 +15,28 @@
         features = "decorations interactive";
       };
     };
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      type = "cat-file -t";
+      dump = "cat-file -p";
+      undo = "reset HEAD~";
+      amend = "commit --amend";
+      unstage = "reset HEAD --";
+      last = "log -1 HEAD";
+      lg = "log --graph --pretty=format:'%C(yellow)%h%Creset %C(green)%ad%Creset | %s%Creset %C(red)%d%Creset %C(bold blue)[%an]%Creset' --date=relative";
+      lga = "log --graph --pretty=format:'%C(yellow)%h%Creset %C(green)%ad%Creset | %s%Creset %C(red)%d%Creset %C(bold blue)[%an]%Creset' --date=relative --all";
+      ls = "ls-files";
+      prune = "remote prune origin";
+      pristine = "reset --hard && clean -dfx";
+      count = "shortlog -sn";
+      cp = "cherry-pick";
+      co-author = "!f() { git commit --author \"$1 <$2>\" -m \"$3\"; }; f";
+      amend-author = "!f() { git commit --amend --author \"$1 <$2>\" -m \"$3\"; }; f";
+      authors = "!git log --format='%aN <%aE>' | sort -u";
+    };
     extraConfig = {
       core = {
         editor = "nvim";
