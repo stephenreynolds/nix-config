76c3ead7f845ba46079e178af516b8ad19144a5f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-25 02:01:17 -0400

    refactor(pkgs): flatten pkgs directory

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,15 +1,15 @@
 { pkgs ? (import ../nixpkgs.nix) { } }: {
   # Packages with a source
-  qt6gtk2 = pkgs.qt6Packages.callPackage ./qt6gtk2 { };
-  primary-xwayland = pkgs.callPackage ./primary-xwayland { };
-  apple-fonts = pkgs.callPackage ./apple-fonts { };
+  qt6gtk2 = pkgs.qt6Packages.callPackage ./qt6gtk2.nix { };
+  primary-xwayland = pkgs.callPackage ./primary-xwayland.nix { };
+  apple-fonts = pkgs.callPackage ./apple-fonts.nix { };
   tastytrade = pkgs.callPackage ./tastytrade.nix { };
 
   # Custom packages
-  sway-audio-idle-inhibit = pkgs.callPackage ./sway-audio-idle-inhibit { };
+  sway-audio-idle-inhibit = pkgs.callPackage ./sway-audio-idle-inhibit.nix { };
   t = pkgs.callPackage ./t { };
   tt = pkgs.callPackage ./tt { };
-  allusion = pkgs.callPackage ./allusion { };
-  segoe-fluent-icons = pkgs.callPackage ./segoe-fluent-icons { };
-  ttf-ms-win11-auto = pkgs.callPackage ./ttf-ms-win11-auto { };
+  allusion = pkgs.callPackage ./allusion.nix { };
+  segoe-fluent-icons = pkgs.callPackage ./segoe-fluent-icons.nix { };
+  ttf-ms-win11-auto = pkgs.callPackage ./ttf-ms-win11-auto.nix { };
 }
