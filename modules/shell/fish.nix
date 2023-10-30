1dca28536fc80a988dbc1cdcfdac1340b9155a8b
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-30 11:49:56 -0400

    fix(cli/fish): remove unused code

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -20,16 +20,7 @@ in {
     {
       hm.programs.fish = {
         enable = true;
-        plugins = [{
-          name = "foreign-env";
-          src = pkgs.fetchFromGitHub {
-            owner = "lilyball";
-            repo = "nix-env.fish";
-            rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
-            sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
-          };
-        }];
-        shellAbbrs = rec {
+        shellAbbrs = {
           jqless = "jq -C | less -r";
 
           n = "nix";
@@ -58,10 +49,6 @@ in {
           e = lib.mkIf config.programs.neovim.enable "nvim";
 
           g = lib.mkIf config.programs.lazygit.enable "lazygit";
-
-          cik = lib.mkIf config.programs.kitty.enable
-            "clone-in-kitty --type os-window";
-          ck = cik;
         };
         shellAliases = {
           # Clear screen and scrollback
@@ -75,13 +62,6 @@ in {
           # Open command buffer in vim when alt+e is pressed
           ''
             bind \ee edit_command_buffer
-          '' +
-          # kitty integration
-          lib.optionalString config.programs.kitty.enable ''
-            set --global KITTY_INSTALLATION_DIR "${pkgs.kitty}/lib/kitty"
-            set --global KITTY_SHELL_INTEGRATION enabled
-            source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
-            set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
           '' +
           # Use vim bindings and cursors
           ''
