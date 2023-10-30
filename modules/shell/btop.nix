89378377bc68f78892d6680eb78b9ffe97dc7f9f
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-30 13:51:22 -0400

    feat(shell/btop): add btop

---
+++ modules/shell/btop.nix
@@ -0,0 +1,30 @@
+{ config, lib, ... }:
+with lib;
+let cfg = config.modules.cli.btop;
+in {
+  options.modules.cli.btop = {
+    enable = mkEnableOption "Enable btop";
+    theme = {
+      theme = mkOption {
+        type = types.str;
+        default = "TTY";
+        description = "Color theme to use";
+      };
+      background = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Use background color";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    hm.programs.btop = {
+      enable = true;
+      settings = {
+        color_theme = cfg.theme.theme;
+        theme_background = cfg.theme.background;
+      };
+    };
+  };
+}
