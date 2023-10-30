4e77c08aeeace1c4246538140b9417b7d73b190e
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-30 13:26:54 -0400

    feat(cli/fish): enable fish home manager module

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -17,6 +17,36 @@ in {
         };
       };
     }
-    { hm.programs.fish = { enable = true; }; }
+    {
+      hm.programs.fish = {
+        enable = true;
+        shellAbbrs = {
+          jqless = "jq -C | less -r";
+
+          n = "nix";
+          nd = "nix develop -c $SHELL";
+          ns = "nix shell";
+          nsn = "nix shell nixpkgs#";
+          nb = "nix build";
+          nbn = "nix build nixpkgs#";
+          nf = "nix flake";
+          nfu = "nix flake update";
+
+          nr = "nixos-rebuild --flake .";
+          nrs = "nixos-rebuild --flake . switch";
+          nrb = "nixos-rebuild --flake . boot";
+          snr = "sudo nixos-rebuild --flake .";
+          snrs = "sudo nixos-rebuild --flake . switch";
+          snrb = "sudo nixos-rebuild --flake . boot";
+          hm = "home-manager --flake .";
+          hms = "home-manager --flake . switch";
+
+          run = "nix run nixpkgs#";
+
+          lsa = "lsd -a";
+          tree = "lsd --tree";
+        };
+      };
+    }
   ]);
 }
