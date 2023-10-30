c4eadc804fcd8423257c86d69bc751b8d88a8224
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-30 17:25:40 -0400

    feat(services/ssh): authenticate using ed25519 key

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -13,6 +13,23 @@ in {
   config = mkIf cfg.enable {
     services.openssh = {
       enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+        StreamLocalBindUnlink = "yes";
+        GatewayPorts = "clientspecified";
+      };
+      hostKeys = [{
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }];
+    };
+
+    programs.ssh = {
+      knownHosts = builtins.mapAttrs (name: _: {
+        publicKeyFile = pubKey name;
+        extraHostNames = (lib.optional (name == hostName) "localhost");
+      }) hosts;
     };
 
     security.pam.enableSSHAgentAuth = true;
