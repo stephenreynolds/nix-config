550c7750615913c69040403b4cac9922441b934e
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-30 18:34:29 -0400

    feat(system/networking): add tcp optimizations

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -17,6 +17,20 @@ in {
       default = true;
       description = "Whether to enable the firewall";
     };
+    optimizations = {
+      tcp = {
+        bbr = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Enable TCP BBR congestion control";
+        };
+        fastOpen = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Enable TCP Fast Open";
+        };
+      };
+    };
   };
 
   config = mkMerge [
@@ -32,6 +46,17 @@ in {
           macAddress = if cfg.randomizeMac then "random" else "preserve";
         };
       };
+    })
+
+    (mkIf cfg.optimizations.tcp.bbr {
+      boot.kernel.sysctl = {
+        "net.core.default_qdisc" = "fq";
+        "net.ipv4.tcp_congestion_control" = "bbr";
+      };
+    })
+
+    (mkIf cfg.optimizations.tcp.fastOpen {
+      boot.kernel.sysctl = { "net.ipv4.tcp_fastopen" = 3; };
     })
   ];
 }
