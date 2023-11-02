2aac6890323906d74a1bca769b4f2e290495c84c
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-02 11:41:18 -0400

    feat(services/flatpak): Add flatpak

---
+++ modules/services/flatpak.nix
@@ -0,0 +1,10 @@
+{ config, lib, ... }:
+with lib;
+let cfg = config.modules.services.flatpak;
+in {
+  options.modules.services.flatpak = {
+    enable = mkEnableOption "Whether to enable Flatpak";
+  };
+
+  config = mkIf cfg.enable { services.flatpak.enable = true; };
+}
