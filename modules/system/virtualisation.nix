220b49688afc40a319419d088271aa5acce444e8
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-04 13:47:58 -0400

    feat(system/virtualisation): add host features

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -3,6 +3,19 @@ with lib;
 let cfg = config.modules.system.virtualisation;
 in {
   options.modules.system.virtualisation = {
+    host = {
+      enable = mkEnableOption "Enable libvirtd for hosting virtual machines";
+      swtpm = mkOption {
+        type = types.bool;
+        default = cfg.host.enable;
+        description = "Enable swtpm for QEMU";
+      };
+      spiceUSBRedirection = mkOption {
+        type = types.bool;
+        default = cfg.host.enable;
+        description = "Enable USB redirection for Spice";
+      };
+    };
     guest = {
       spice = mkEnableOption "Enable the Spice agent";
       qxl = mkEnableOption "Enable the QXL video driver";
@@ -11,6 +24,34 @@ in {
   };
 
   config = mkMerge [
+    (mkIf cfg.host.enable {
+      virtualisation = {
+        libvirtd = {
+          enable = true;
+          qemu = {
+            swtpm.enable = cfg.host.swtpm;
+            ovmf = {
+              enable = true;
+              packages = [ pkgs.OVMFFull.fd ];
+            };
+          };
+        };
+        spiceUSBRedirection.enable = cfg.host.spiceUSBRedirection;
+      };
+
+      services.spice-vdagentd.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        virt-manager
+        virt-viewer
+        spice
+        spice-gtk
+        spice-protocol
+      ];
+
+      programs.dconf.enable = true;
+    })
+
     (mkIf cfg.guest.spice { services.spice-vdagentd.enable = true; })
 
     (mkIf cfg.guest.qxl { services.xserver.videoDrivers = [ "qxl" ]; })
