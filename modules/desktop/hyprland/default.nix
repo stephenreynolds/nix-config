175f60a3372d386f0b5cb634e8f61f4f43f0c098
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-10 00:14:45 -0500

    style(hyprland): formatting

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -100,8 +100,7 @@ in
         Unit = {
           Description = "Hyprland compositor session";
           BindsTo = [ "graphical-session.target" ];
-          Wants =
-            [ "graphical-session-pre.target" "xdg-desktop-autostart.target" ];
+          Wants = [ "graphical-session-pre.target" "xdg-desktop-autostart.target" ];
           After = [ "graphical-session-pre.target" ];
           Before = [ "xdg-desktop-autostart.target" ];
         };
