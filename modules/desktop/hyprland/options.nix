69b1cd09746466734fefa193ecd72a945973d641
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-11 16:57:49 -0500

    feat(hyprland): disable rendering of logo and splash

--- modules/desktop/hyprland/options.nix
+++ modules/desktop/hyprland/options.nix
@@ -87,6 +87,8 @@ in mkIf config.modules.desktop.hyprland.enable {
     }
 
     misc {
+      disable_hyprland_logo = true
+      disable_splash_rendering = true
       focus_on_activate = true
       mouse_move_enables_dpms = true
       key_press_enables_dpms = true
