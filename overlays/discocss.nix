3320ab25bf9d325360e5308664b062def3f4fbd0
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-10 23:17:12 -0500

    feat(discord): replace discocss diff with a fork

--- overlays/discocss.nix
+++ overlays/discocss.nix
@@ -1,4 +1,12 @@
 final: prev: {
   discocss = prev.discocss.overrideAttrs
-    (oldAttrs: { patches = (oldAttrs.patches or [ ]) ++ [ ./discocss.diff ]; });
+    (old: rec {
+      version = "0.3.0";
+      src = prev.fetchFromGitHub {
+        owner = "bddvlpr";
+        repo = "discocss";
+        rev = "v${version}";
+        hash = "sha256-2K7SPTvORzgZ1ZiCtS5TOShuAnmtI5NYkdQPRXIBP/I=";
+      };
+    });
 }
