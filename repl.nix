4312df4c4ffc5533edee0b7650a3dbeaf317e1fb
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-15 05:27:29 -0500

    feat(repl): add repl.nix

---
+++ repl.nix
@@ -0,0 +1,9 @@
+let
+  flake = builtins.getFlake (toString ./.);
+  inherit (flake.inputs) nixpkgs;
+  pkgs = import flake.inputs.nixpkgs { };
+in
+flake // {
+  inherit flake pkgs;
+  inherit (nixpkgs) lib;
+}
