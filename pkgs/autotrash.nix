155e17c2ff407531e9b4b75a037a72637ec12b5e
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-11 07:33:01 -0500

    style(pkgs/autotrash): add line spacing

--- pkgs/autotrash.nix
+++ pkgs/autotrash.nix
@@ -3,11 +3,15 @@
 python3Packages.buildPythonPackage rec {
   pname = "autotrash";
   version = "0.4.6";
+
   src = fetchPypi {
     inherit pname version;
     sha256 = "sha256-5h178yDqrtnkwx5xBoGmJEJ8MZGhI1FpmqjLOI/rO2A=";
   };
+
   format = "pyproject";
+
   propagatedBuildInputs = [ python3Packages.poetry-core ];
+
   doCheck = false;
 }
