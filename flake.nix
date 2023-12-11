b7973781ff2aa8e3baf374fe33fd9fa46ed39e71
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-11 07:50:51 -0500

    fix(apps/obsidian): temporarily permit old electron version

--- flake.nix
+++ flake.nix
@@ -82,6 +82,10 @@
         import pkgs {
           inherit system;
           config.allowUnfree = true;
+          # HACK: fixes obsidian until its version of electron is updated
+          config.permittedInsecurePackages = [
+            "electron-25.9.0"
+          ];
           overlays = extraOverlays ++ (builtins.attrValues self.overlays);
         };
       pkgs = mkPkgs nixpkgs [ self.overlays.default ];
