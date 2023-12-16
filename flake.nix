1facb2b26a8c7831462c508cd8a32346a25a7e47
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-16 04:21:54 -0500

    refactor(flake): remove hyprland-plugins input

--- flake.nix
+++ flake.nix
@@ -38,10 +38,6 @@
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
