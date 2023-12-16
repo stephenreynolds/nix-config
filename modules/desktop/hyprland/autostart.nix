5e0c9293942def20c21bb831c63882eb06a842df
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-16 17:58:26 -0500

    fix(hyprland/autostart): fix ags not quitting before restarting

--- modules/desktop/hyprland/autostart.nix
+++ modules/desktop/hyprland/autostart.nix
@@ -4,12 +4,12 @@ let
   cfg = config.modules.desktop.hyprland;
 
   primaryMonitor = lib.findSingle (m: m.primary) "DP-1" "DP-1" config.modules.devices.monitors;
-  ags = "${config.hm.programs.ags.package}/bin/ags";
+  ags = "${config.hm.programs.ags.package}/bin/ags -b hyprland";
 in
 lib.mkIf cfg.enable {
   hm.wayland.windowManager.hyprland.settings = {
     exec = [
-      "${ags} -q; sleep 0.5; ${ags} -b hyprland"
+      "${ags} -q; sleep 0.5; ${ags}"
     ];
 
     "exec-once" = [
