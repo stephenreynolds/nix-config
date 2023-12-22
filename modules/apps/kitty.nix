1c0dedd3f0896498a3aff8faa37f65aa84638fb9
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-22 00:05:13 -0500

    feat(apps/kitty): use catppuccin mocha for text

--- modules/apps/kitty.nix
+++ modules/apps/kitty.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let
   cfg = config.modules.apps.kitty;
@@ -7,6 +7,7 @@ let
   '';
 
   colorscheme = config.modules.desktop.theme.colorscheme;
+  text-colors = inputs.nix-colors.colorSchemes.catppuccin-mocha;
 in
 {
   options.modules.apps.kitty = {
@@ -74,45 +75,52 @@ in
     }
 
     (lib.mkIf (colorscheme != null) {
-      hm.programs.kitty.settings =
-        let inherit (colorscheme) colors;
-        in {
-          foreground = "#${colors.base05}";
-          background = "#${colors.base00}";
-          selection_background = "#${colors.base05}";
-          selection_foreground = "#${colors.base00}";
-          url_color = "#${colors.base04}";
-          cursor = "#${colors.base05}";
-          active_border_color = "#${colors.base03}";
-          inactive_border_color = "#${colors.base01}";
-          active_tab_background = "#${colors.base00}";
-          active_tab_foreground = "#${colors.base05}";
-          inactive_tab_background = "#${colors.base01}";
-          inactive_tab_foreground = "#${colors.base04}";
-          tab_bar_background = "#${colors.base01}";
-          color0 = "#${colors.base00}";
-          color1 = "#${colors.base08}";
-          color2 = "#${colors.base0B}";
-          color3 = "#${colors.base0A}";
-          color4 = "#${colors.base0D}";
-          color5 = "#${colors.base0E}";
-          color6 = "#${colors.base0C}";
-          color7 = "#${colors.base05}";
-          color8 = "#${colors.base03}";
-          color9 = "#${colors.base08}";
-          color10 = "#${colors.base0B}";
-          color11 = "#${colors.base0A}";
-          color12 = "#${colors.base0D}";
-          color13 = "#${colors.base0E}";
-          color14 = "#${colors.base0C}";
-          color15 = "#${colors.base07}";
-          color16 = "#${colors.base09}";
-          color17 = "#${colors.base0F}";
-          color18 = "#${colors.base01}";
-          color19 = "#${colors.base02}";
-          color20 = "#${colors.base04}";
-          color21 = "#${colors.base06}";
-        };
+      hm.programs.kitty.settings = lib.mkMerge [
+        (
+          let inherit (colorscheme) colors; in {
+            background = "#${colors.base00}";
+          }
+        )
+
+        (
+          let inherit (text-colors) colors; in {
+            foreground = "#${colors.base05}";
+            selection_background = "#${colors.base05}";
+            selection_foreground = "#${colors.base00}";
+            url_color = "#${colors.base04}";
+            cursor = "#${colors.base05}";
+            active_border_color = "#${colors.base03}";
+            inactive_border_color = "#${colors.base01}";
+            active_tab_background = "#${colors.base00}";
+            active_tab_foreground = "#${colors.base05}";
+            inactive_tab_background = "#${colors.base01}";
+            inactive_tab_foreground = "#${colors.base04}";
+            tab_bar_background = "#${colors.base01}";
+            color0 = "#${colors.base00}";
+            color1 = "#${colors.base08}";
+            color2 = "#${colors.base0B}";
+            color3 = "#${colors.base0A}";
+            color4 = "#${colors.base0D}";
+            color5 = "#${colors.base0E}";
+            color6 = "#${colors.base0C}";
+            color7 = "#${colors.base05}";
+            color8 = "#${colors.base03}";
+            color9 = "#${colors.base08}";
+            color10 = "#${colors.base0B}";
+            color11 = "#${colors.base0A}";
+            color12 = "#${colors.base0D}";
+            color13 = "#${colors.base0E}";
+            color14 = "#${colors.base0C}";
+            color15 = "#${colors.base07}";
+            color16 = "#${colors.base09}";
+            color17 = "#${colors.base0F}";
+            color18 = "#${colors.base01}";
+            color19 = "#${colors.base02}";
+            color20 = "#${colors.base04}";
+            color21 = "#${colors.base06}";
+          }
+        )
+      ];
     })
   ]);
 }
