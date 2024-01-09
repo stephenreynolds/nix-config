ee112affffa73db7feda13cda7a26d7fea8c5223
Stephen Reynolds <mail@stephenreynolds.dev>
2024-01-08 21:28:01 -0500

    feat(home-manager): configure nix

--- modules/home/nix/default.nix
+++ modules/home/nix/default.nix
@@ -1,11 +1,43 @@
-{
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      # HACK: fixes obsidian until its version of electron is updated
-      permittedInsecurePackages = [
-        "electron-25.9.0"
-      ];
+{ config, lib, pkgs, inputs, ... }:
+
+let cfg = config.modules.nix;
+in {
+  options.modules.nix = {
+    auto-optimise-store = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to automatically optimise the Nix store";
+    };
+    use-cgroups = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to execute builds inside cgroups";
     };
   };
+
+  config = lib.mkMerge [
+    {
+      nix = {
+        package = pkgs.nix;
+        settings = {
+          trusted-users = [ "root" "@wheel" ];
+          experimental-features = [
+            "auto-allocate-uids"
+            "ca-derivations"
+            "cgroups"
+            "flakes"
+            "nix-command"
+          ];
+          warn-dirty = false;
+          inherit (cfg) auto-optimise-store use-cgroups;
+        };
+
+        # Add each flake input as a registry
+        # To make nix3 commands consistent with the flake
+        registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+      };
+
+      nixpkgs.config.allowUnfree = true;
+    }
+  ];
 }
