67efe09cc1bfbc7831421c2c39406300ef8f8d3c
Stephen Reynolds <mail@stephenreynolds.dev>
2024-01-11 22:22:23 -0500

    feat: add gnome-policykit-agent

---
+++ modules/home/services/gnome-policykit-agent.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) types mkOption mkEnableOption mkIf;
+  cfg = config.modules.services.gnome-policykit-agent;
+in
+{
+  options.modules.services.gnome-policykit-agent = {
+    enable = mkEnableOption "GNOME PolicyKit Agent";
+    package = mkOption {
+      type = types.package;
+      default = pkgs.polkit_gnome;
+      defaultText = "pkgs.polkit-gnome";
+      description = "GNOME PolicyKit Agent package to use";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.gnome-policykit-agent = {
+      Unit = {
+        Description = "GNOME PolicyKit Agent";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${cfg.package}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+}
