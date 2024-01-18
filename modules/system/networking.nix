2e1bd14c43bbaf3261b332aabd608146ee8a4db0
Stephen Reynolds <mail@stephenreynolds.dev>
2024-01-18 05:34:28 -0500

    feat(networking): set tcp_mtu_probing

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -86,6 +86,7 @@ in {
       boot.kernel.sysctl = {
         "net.core.default_qdisc" = "fq";
         "net.ipv4.tcp_congestion_control" = "bbr";
+        "net.ipv4.tcp_mtu_probing" = 1;
       };
     })
 
