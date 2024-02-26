0861abc48e5efd0683d7dd65cd8e0acb7cd603b7
Stephen Reynolds <mail@stephenreynolds.dev>
2024-02-26 00:01:06 -0500

    fix(impermanence): workaround to persist trash since it can't be symlink

--- modules/users/xdg-directories.nix
+++ modules/users/xdg-directories.nix
@@ -1,8 +1,46 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   modules.system.persist.state.home.directories = [
-    ".local/share/Trash"
     "Downloads"
   ];
+
+  hm.systemd.user.services.persist-trash = {
+    Unit.Description = "Persist Trash Folder";
+    Install.WantedBy = [ "default.target" ];
+    Service =
+      let
+        dir = ''
+          LOCAL="$HOME/.local/share/Trash"
+          PERSIST="/persist/home/$USER/.local/share/Trash"
+          mkdir -p "$LOCAL"
+        '';
+      in
+      {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        StandardOutput = "journal";
+        ExecStart = "${pkgs.writeShellApplication {
+              name = "retrieve";
+              runtimeInputs = [pkgs.coreutils];
+              text = ''
+                ${dir}
+                if [ -d "$PERSIST" ]
+                then
+                  cp -r "$PERSIST"/. "$LOCAL"
+                  rm -rf "$PERSIST"
+                fi
+              '';
+            }}/bin/retrieve";
+        ExecStop = "${pkgs.writeShellApplication {
+              name = "migrate";
+              runtimeInputs = [pkgs.coreutils];
+              text = ''
+                ${dir}
+                mkdir -p "$PERSIST"
+                cp -r "$LOCAL"/. "$PERSIST"
+              '';
+            }}/bin/migrate";
+      };
+  };
 }
