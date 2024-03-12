0e9f8f83bdba9b00f3b0e91e4b3ce3be93d56b02
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-12 16:29:00 -0400

    refactor(hyprland): remove redundant autostart script

--- modules/desktop/hyprland/autostart.nix
+++ modules/desktop/hyprland/autostart.nix
@@ -4,14 +4,9 @@ let
   cfg = config.modules.desktop.hyprland;
 
   primaryMonitor = lib.findSingle (m: m.primary) "DP-1" "DP-1" config.modules.devices.monitors;
-  ags = "${config.hm.programs.ags.package}/bin/ags -b hyprland";
 in
 lib.mkIf cfg.enable {
   hm.wayland.windowManager.hyprland.settings = {
-    exec = [
-      "${ags} -q; ${ags}"
-    ];
-
     "exec-once" = [
       "hyprctl dispatch focusmonitor ${primaryMonitor.name}"
     ];
