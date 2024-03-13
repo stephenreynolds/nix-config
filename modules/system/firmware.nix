7c60519d6e7c95032a3812f92a75bbd3f5336a96
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 19:03:00 -0400

    feat(system): add fwupd

---
+++ modules/system/firmware.nix
@@ -0,0 +1,19 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkOption types;
+  cfg = config.modules.system.firmware;
+in
+{
+  options.modules.system.firmware = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to allow applications to update firmwar";
+    };
+  };
+
+  config = {
+    services.fwupd.enable = cfg.enable;
+  };
+}
