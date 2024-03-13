bfaee91007667675d87457d8105787892d67b3a8
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 16:33:14 -0400

    feat(vesktop): add autostart .desktop file

--- modules/apps/vesktop.nix
+++ modules/apps/vesktop.nix
@@ -1,17 +1,37 @@
 { config, lib, pkgs, ... }:
 
 let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkOption mkEnableOption mkIf mkMerge types;
   cfg = config.modules.apps.vesktop;
 in
 {
   options.modules.apps.vesktop = {
     enable = mkEnableOption "Whether to install Vesktop, a Discord client";
+    autostart = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to start Vesktop on login";
+    };
   };
 
-  config = mkIf cfg.enable {
-    hm.home.packages = [ pkgs.vesktop ];
+  config = mkIf cfg.enable (mkMerge [
+    {
+      hm.home.packages = [ pkgs.vesktop ];
 
-    modules.system.persist.state.home.directories = [ ".config/vesktop" ];
-  };
+      modules.system.persist.state.home.directories = [ ".config/vesktop" ];
+    }
+
+    (mkIf cfg.autostart {
+      hm.xdg.configFile."autostart/vencord.desktop".text = ''
+        [Desktop Entry]
+        Type=Application
+        Version=1.0
+        Name=Vencord
+        Comment=Vencord autostart script
+        Exec=${pkgs.vesktop}/bin/vesktop
+        Terminal=false
+        StartupNotify=false
+      '';
+    })
+  ]);
 }
