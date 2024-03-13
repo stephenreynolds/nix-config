26c031877d05f92cfc69c29dc5060a9248af9531
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 19:25:45 -0400

    fix(hyprland): disable suspend on idle

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -32,6 +32,7 @@ in
         enable = true;
         xdg-autostart = cfg.xdg-autostart;
         primaryMonitor = (findSingle (m: m.primary) "DP-1" "DP-1" config.modules.devices.monitors).name;
+        hypridle.suspend.enable = false;
       };
 
       modules.system.persist.cache.home.directories = [ ".cache/ags/user" ];
