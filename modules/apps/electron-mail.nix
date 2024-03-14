fa3788a5988863125450a6cbea211bb2f6dcc449
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-13 23:36:32 -0400

    feat(apps/electron-mail): add autostart script

--- modules/apps/electron-mail.nix
+++ modules/apps/electron-mail.nix
@@ -1,17 +1,20 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.modules.apps.electron-mail;
-in {
+let
+  inherit (lib) mkOption mkEnableOption mkIf types;
+  cfg = config.modules.apps.electron-mail;
+in
+{
   options.modules.apps.electron-mail = {
-    enable = lib.mkEnableOption "Whether to install ElectronMail";
-    autostart = lib.mkOption {
-      type = lib.types.bool;
+    enable = mkEnableOption "Whether to install ElectronMail";
+    autostart = mkOption {
+      type = types.bool;
       default = true;
       description = "Whether to start ElectronMail on login";
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     hm.home.packages = [ pkgs.my.electron-mail ];
 
     hm.xdg.configFile."autostart/electron-mail.desktop" = {
@@ -19,10 +22,10 @@ in {
       text = ''
         [Desktop Entry]
         Type=Application
-        Exec=gtk-launch electron-mail
+        Exec=${pkgs.my.electron-mail}/bin/electron-mail
         Terminal=false
         StartupWMClass=electron-mail
-        X-AppImage-Version=5.1.8
+        X-AppImage-Version=5.2.2
         Name=ElectronMail
         Icon=electron-mail
         Comment=Unofficial ProtonMail Desktop App
