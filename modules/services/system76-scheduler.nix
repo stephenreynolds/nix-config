70e5bbb988d25cc4632416f10bfe94858b2e1a65
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-14 19:13:29 -0400

    feat(system76-scheduler): add hyprland to desktop environments

--- modules/services/system76-scheduler.nix
+++ modules/services/system76-scheduler.nix
@@ -1,10 +1,17 @@
 { config, lib, ... }:
 
-let cfg = config.modules.services.system76-scheduler;
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.services.system76-scheduler;
 in {
   options.modules.services.system76-scheduler = {
-    enable = lib.mkEnableOption "Whether to enable system76-scheduler";
+    enable = mkEnableOption "Whether to enable system76-scheduler";
   };
 
-  config = lib.mkIf cfg.enable { services.system76-scheduler.enable = true; };
+  config = mkIf cfg.enable {
+    services.system76-scheduler = {
+      enable = true;
+      assignments.desktop-environment.matchers = [ "Hyprland" ];
+    };
+  };
 }
