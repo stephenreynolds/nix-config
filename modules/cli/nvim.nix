e5301a0260b9dfc9777ca47514c5abc77d0c7517
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-16 03:13:21 -0400

    style: formatting

--- modules/cli/nvim.nix
+++ modules/cli/nvim.nix
@@ -1,28 +1,30 @@
 { config, lib, inputs, ... }:
 
-let cfg = config.modules.cli.nvim;
+let
+  inherit (lib) mkEnableOption mkOption mkIf types;
+  cfg = config.modules.cli.nvim;
 in {
   options.modules.cli.nvim = {
-    enable = lib.mkEnableOption "Enable Neovim";
-    defaultEditor = lib.mkEnableOption "Set Neovim as default editor";
-    viAlias = lib.mkOption {
-      type = lib.types.bool;
+    enable = mkEnableOption "Enable Neovim";
+    defaultEditor = mkEnableOption "Set Neovim as default editor";
+    viAlias = mkOption {
+      type = types.bool;
       default = true;
       description = "Create vi alias";
     };
-    vimAlias = lib.mkOption {
-      type = lib.types.bool;
+    vimAlias = mkOption {
+      type = types.bool;
       default = true;
       description = "Create vim alias";
     };
-    vimdiffAlias = lib.mkOption {
-      type = lib.types.bool;
+    vimdiffAlias = mkOption {
+      type = types.bool;
       default = true;
       description = "Create vimdiff alias";
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     hm.imports = [ inputs.nvim-config.homeManagerModules.default ];
 
     hm.programs.neovim = {
