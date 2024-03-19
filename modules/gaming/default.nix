45b639ba8ad1358df29c98ab2bf20330a884dd2e
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-18 22:56:31 -0400

    style(gaming): inherit lib functions

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -1,58 +1,59 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.modules.gaming;
+let
+  inherit (lib) mkOption mkEnableOption mkIf mkMerge mkDefault types;
+  cfg = config.modules.gaming;
 in {
   options.modules.gaming = {
-    enable =
-      lib.mkEnableOption "Whether to enable gaming-related fixes and tools";
+    enable = mkEnableOption "Whether to enable gaming-related fixes and tools";
     memory-fix = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
+      enable = mkOption {
+        type = types.bool;
         default = cfg.enable;
         description = "Whether to enable memory fix for some games";
       };
     };
     gamemode = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
+      enable = mkOption {
+        type = types.bool;
         default = cfg.enable
           && !config.modules.services.system76-scheduler.enable;
         description = "Whether to enable GameMode";
       };
     };
     gamescope = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
+      enable = mkOption {
+        type = types.bool;
         default = cfg.enable;
         description = "Whether to enable Gamescope";
       };
     };
     mangohud = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
+      enable = mkOption {
+        type = types.bool;
         default = cfg.enable;
         description = "Whether to install MangoHud";
       };
     };
   };
 
-  config = lib.mkMerge [
+  config = mkMerge [
     {
-      modules.system.pipewire.lowLatency = lib.mkDefault true;
+      modules.system.pipewire.lowLatency = mkDefault true;
 
       systemd.extraConfig = "DefaultLimitNOFILE=1048576";
 
       environment.sessionVariables = { WINEDEBUG = "-all"; };
     }
 
-    (lib.mkIf cfg.memory-fix.enable {
+    (mkIf cfg.memory-fix.enable {
       boot.kernel.sysctl = {
         "vm.max_map_count" = 2147483642;
         "kernel.split_lock_mitigate" = 0;
       };
     })
 
-    (lib.mkIf cfg.gamemode.enable {
+    (mkIf cfg.gamemode.enable {
       programs.gamemode = {
         enable = true;
         enableRenice = true;
@@ -68,13 +69,13 @@ in {
       users.groups.gamemode = { };
     })
 
-    (lib.mkIf cfg.gamescope.enable {
+    (mkIf cfg.gamescope.enable {
       programs.gamescope = {
         enable = true;
         capSysNice = true;
       };
     })
 
-    (lib.mkIf cfg.mangohud.enable { hm.home.packages = [ pkgs.mangohud ]; })
+    (mkIf cfg.mangohud.enable { hm.home.packages = [ pkgs.mangohud ]; })
   ];
 }
