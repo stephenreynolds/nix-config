f7ed9225d67b68d375f92e8690ccb711bbd7ef87
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-07 00:23:17 -0400

    feat(apps/vesktop): start vesktop minimized

--- modules/apps/vesktop.nix
+++ modules/apps/vesktop.nix
@@ -3,7 +3,8 @@
 let
   inherit (lib) mkOption mkEnableOption mkIf mkMerge types;
   cfg = config.modules.apps.vesktop;
-in {
+in
+{
   options.modules.apps.vesktop = {
     enable = mkEnableOption "Whether to install Vesktop, a Discord client";
     autostart = mkOption {
@@ -21,13 +22,13 @@ in {
     }
 
     (mkIf cfg.autostart {
-      hm.xdg.configFile."autostart/vencord.desktop".text = ''
+      hm.xdg.configFile."autostart/vesktop.desktop".text = ''
         [Desktop Entry]
         Type=Application
         Version=1.0
         Name=Vencord
         Comment=Vencord autostart script
-        Exec=${pkgs.vesktop}/bin/vesktop
+        Exec=sh -c "${pkgs.vesktop}/bin/vesktop --start-minimized"
         Terminal=false
         StartupNotify=false
       '';
