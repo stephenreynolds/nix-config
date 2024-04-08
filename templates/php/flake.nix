0bac24ad47df89ab7fde105666cc0fe458240e6e
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-08 17:42:13 -0400

    templates(php): add php template

---
+++ templates/php/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "PHP project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      phpVersion = 83;
+      overlays = [
+        (final: prev: {
+          php = prev."php${toString phpVersion}".buildEnv {
+            extensions = ({ enabled, all }: enabled ++ (with all; [
+              # Extensions here
+            ]));
+            extraConfig = ''
+            '';
+          };
+        })
+      ];
+
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+          (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
+    in
+    {
+      devShells = forEachSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            php
+            php.packages.composer
+            nodejs
+          ];
+
+          shellHook = ''
+            export PATH=$HOME/.config/composer/vendor/bin:$PATH
+          '';
+        };
+      });
+    };
+}
