c3c97a9bb49e0829a061533967cecb624dc919c5
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-16 18:29:58 -0400

    Revert "hyprlock: temporarily disable hyprlock"
    This reverts commit b8f1aa532b03ea466aa08060ae65fd9a6adef211.

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -44,11 +44,8 @@ in
         tearing.enable = config.modules.gaming.enable;
         xdg-autostart = cfg.xdg-autostart;
       };
-      hyprlock.enable = false;
     };
 
-    modules.system.persist.cache.home.directories = [
-      ".cache/ags/user"
-    ];
+    modules.system.persist.cache.home.directories = [ ".cache/ags/user" ];
   }]);
 }
