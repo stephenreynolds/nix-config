f3cda5f26f1e16077707bbc91d54e8297b5c92bd
Stephen Reynolds <mail@stephenreynolds.dev>
2024-06-05 23:06:20 -0400

    template: add c template

---
+++ templates/c/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [ gcc clang ];
+        };
+      }
+    );
+}
