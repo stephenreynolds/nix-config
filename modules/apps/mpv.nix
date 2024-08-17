f23a8b65c2a29ed2c27a43ea66cead4235e5b813
Stephen Reynolds <mail@stephenreynolds.dev>
2024-08-16 23:51:18 -0400

    apps(mpv): add option for youtube support

--- modules/apps/mpv.nix
+++ modules/apps/mpv.nix
@@ -1,19 +1,38 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
-let cfg = config.modules.apps.mpv;
-in {
+let
+  inherit (lib) mkEnableOption mkOption mkIf mkMerge types;
+  cfg = config.modules.apps.mpv;
+in
+{
   options.modules.apps.mpv = {
-    enable = lib.mkEnableOption "Whether to install mpv";
-  };
-
-  config = lib.mkIf cfg.enable {
-    hm.programs.mpv = {
-      enable = true;
-      config = {
-        profile = "gpu-hq";
-        vo = "gpu";
-        hwdec = "auto-safe";
-      };
+    enable = mkEnableOption "Whether to install mpv";
+    youtube = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to enable YouTube support";
     };
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      hm.programs.mpv = {
+        enable = true;
+        scripts = with pkgs.mpvScripts; [
+          mpris
+          mpv-playlistmanager
+          quality-menu
+        ];
+        config = {
+          profile = "gpu-hq";
+          vo = "gpu";
+          hwdec = "auto-safe";
+        };
+      };
+    }
+
+    (mkIf cfg.youtube {
+      hm.home.packages = [ pkgs.yt-dlp ];
+    })
+  ]);
 }
